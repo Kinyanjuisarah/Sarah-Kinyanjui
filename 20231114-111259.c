5dcdba2a10dfa23c33cfc54a0795b869f9dd77a1
Kinyanjuisarah <150795048+Kinyanjuisarah@users.noreply.github.com>
2023-11-14 13:32:25 +0300

    Add files via upload

---
+++ 20231114-111259.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+int main()
+{
+/*A program to store and manage student details*/
+
+char registrationNumber[30];
+char firstName[30];
+char lastName[25];
+float intro_To_Programming;
+float networking;
+float accounting;
+float operatingSystem;
+float computerApplication;
+
+ printf("enter your registrationNumber\n");
+ scanf("%s",&registrationNumber);
+ 
+ printf("enter your firstName\n");
+ scanf("%s",&firstName);
+ 
+ printf("enter your lastName\n");
+ scanf("%s",&lastName);
+ 
+ printf("intro_To_Programming\n");
+ scanf("%f",&intro_To_Programming);
+ 
+ printf("networking\n");
+ scanf("%f",&networking);
+ 
+ 
+ printf("operatingSystem\n");
+ scanf("%f",&operatingSystem);
+ 
+ printf("computerApplication\n");
+ scanf("%f",&computerApplication);
+ 
+ return 0;
+} 
+ 
+ 
+ 
+ 
